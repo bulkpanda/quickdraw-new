805b279b50934f4cd21abe38444e5f710eb9bf02
Kunal Patel <kunal.patel0999@gmail.com>
2022-11-16 01:03:46 +1100

    added server configuration for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //SERVER_URL:"https://kunal.autonise.com/api"
-  SERVER_URL:"http://54.200.234.219/api"
+  SERVER_URL:"http://54.200.234.219/api"         //public ip of server/api
 };
