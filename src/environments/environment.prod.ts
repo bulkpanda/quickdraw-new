e14d5021d9a81fa20557f9b1b3c12b32e5f7bec0
Kunal Patel <kunal.patel0999@gmail.com>
2022-11-16 00:40:10 +1100

    changed env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //SERVER_URL:"https://kunal.autonise.com/api"
-  SERVER_URL:"http://127.0.0.1:5000/api"
+  SERVER_URL:"http://54.200.234.219/api"
 };
